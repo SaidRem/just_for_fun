a25097e33c72e521071c247e2aebb3fa296f724d
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-07-03 23:18:29 +0400

    Add files via upload

---
+++ alphabet_rangoli.py
@@ -0,0 +1,47 @@
+'''
+# size 3
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 10
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+
+'''
+
+
+def print_rangoli(n):
+    for i in range(n):
+        s = '-'.join(chr(ord('a')+n-j-1) for j in range(i+1))
+        print((s+s[::-1][1:]).center(n*4-3, '-'))
+
+    for i in range(n-1):
+        s = '-'.join(chr(ord('a')+n-j-1) for j in range(n-i-1))
+        print((s+s[::-1][1:]).center(n*4-3, '-'))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
