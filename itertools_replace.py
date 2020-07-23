80e41ca4ee3b39a5dff69667a4ff3e544a5bc611
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-07-23 22:11:15 +0400

    Add files via upload

---
+++ itertools_replace.py
@@ -0,0 +1,10 @@
+# Given a string.
+# Print all possible size replacement combinations of the string in
+# lexicographic sorted order.
+
+# Input format:
+# A sinfle line containing the string and integer value separated by a space.
+
+# Output format:
+# Print the combinations with their replacements of the string on separate lines.
+
