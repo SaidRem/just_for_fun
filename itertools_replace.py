9e06560ee22072f890a8dbba4750385d5f9fd487
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-07-23 22:25:42 +0400

    Add files via upload

--- itertools_replace.py
+++ itertools_replace.py
@@ -8,3 +8,13 @@
 # Output format:
 # Print the combinations with their replacements of the string on separate lines.
 
+from itertools import combinations_with_replacement
+# This tool return r lingth subsequences of elements of elements from
+# the input iterable allowing individual elements to be repeated more
+# than once.
+string, n = input().split()
+
+combinations = combinations_with_replacement(sorted(string), int(n))
+
+for row in combinations:
+    print(''.join(row))
