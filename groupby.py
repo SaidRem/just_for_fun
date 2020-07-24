e0e4043dacfb9953c3b6a6e3d973d84bcde6b03a
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-07-24 23:23:44 +0400

    Update groupby.py

--- groupby.py
+++ groupby.py
@@ -11,6 +11,23 @@
 
 from itertools import groupby
 
+# itertools.groupby(iterable[, key])
+# groupby returns an iterator that returns consecutive keys and groups from
+# the iterable. The key is a function computing a key value for each element.
+# if not specified or is None, key defaults to an identity function and returns
+# the element unchanged.
+# vehicles = [('Ford', 'Taurus'), ('Dodge', 'Durango'),
+#             ('Chevrolet', 'Cobalt'), ('Ford', 'F150'),
+#             ('Dodge', 'Charger'), ('Ford', 'GT')]
+
+# sorted_vehicles = sorted(vehicles)
+
+# for key, group in groupby(sorted_vehicles, lambda make: make[0]):
+#     for make, model in group:
+#         print('{model} is made by {make}'.format(model=model, make=make))
+
+#     print ("**** END OF GROUP ***\n")
+
 the_line = input()
 
 print(*[(len(list(iter_string)), int(c))
