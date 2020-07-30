7453f282f21db68b10dc35448cd264352e4dcb64
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-07-30 22:19:01 +0400

    Add files via upload

---
+++ triangle_quest.py
@@ -0,0 +1,19 @@
+# Input format:
+# A single line containing integer N.
+
+# Output format:
+# Print a numerical triangle of height N - 1 like the one below:
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+for i in range(1, int(input())):
+    print((10**(i) // 9) * i)
+
+# Another version:
+
+# for i in range(1, int(input())):
+#     print([0, 1, 22, 333, 4444, 55555, 666666,
+#            7777777, 88888888, 999999999][i])
