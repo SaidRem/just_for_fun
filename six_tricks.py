e103e6eed91fb06d74153d3e8ecfe4a3591f3df4
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-09-27 21:32:05 +0400

    Update six_tricks.py

--- six_tricks.py
+++ six_tricks.py
@@ -1,10 +1,10 @@
-# 1. List comprehension
+# 1. List comprehension.
 
 from random import randint
 my_list = [randint(1, 10) for _ in range(1, 1000)]
 # print(my_list)
 
-# 2 Zip
+# 2 Zip function.
 
 names = ['Alice', 'Bob', 'Josh', 'Erika', 'Maria']
 ages = [23, 30, 43, 18, 20]
@@ -13,7 +13,7 @@ container = zip(names, ages, likes)
 for c in container:
     print('{} is {} year old and likes {}'.format(*c))
 
-# 3. Counter 
+# 3. Counter class from collections module.
 
 from collections import Counter
 
@@ -30,12 +30,12 @@ my_counter = Counter(my_list)
 for c in my_counter.most_common(3):
     print(f'{c[0]}: {c[1]} occurences.')
 
-# 4. Enumerating
+# 4. Enumerating function.
 
 for i, customer in enumerate(names, 1):
     print(f'{i} : {customer}')
 
-# 5. Parameter expansion
+# 5. Parameter expansion.
 
 
 def proc_stud_info(name, last_n, fav_topic, score):
@@ -48,8 +48,9 @@ proc_stud_info(*student)
 student = dict(name='Alice', last_n='Smith', fav_topic='math', score='A')
 proc_stud_info(**student)
 
-# 6. Type annotation
+# 6. Type annotation.
 
 
 def some_computations(a: int, b: int):
+    """Makes some computations"""
     return a + b
